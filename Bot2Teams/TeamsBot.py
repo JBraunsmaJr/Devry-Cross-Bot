bf064b24ae856c85174eb8ec38a35b0833200226
Jon Braunsma <jonbraunsma@gmail.com>
2021-01-24 01:40:00 -0500

    Updated requirements.txt Added template code

--- Bot2Teams/TeamsBot.py
+++ Bot2Teams/TeamsBot.py
@@ -3,7 +3,25 @@
 Bot for Teams
 """
 
-class TeamsBot:
+from botbuilder.core import ActivityHandler, TurnContext
+from botbuilder.schema import ChannelAccount
+
+
+class TeamsBot(ActivityHandler):
     def __init__(self):
         pass
-    
+
+    async def on_message_activity(  # pylint: disable=unused-argument
+        self, turn_context: TurnContext
+    ):
+        await turn_context.send_activity(f"You said '{turn_context.activity.text}'")
+
+    async def on_members_added_activity(
+        self,
+        members_added: ChannelAccount,
+        turn_context: TurnContext
+    ):
+        for member_added in members_added:
+            if member_added.id != turn_context.activity.recipient.id:
+                await turn_context.send_activity("Hello and welcome!")
+
